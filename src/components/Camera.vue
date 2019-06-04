edfcc69221bcc315b9bc63ed3be9732b91b1e024
Ivan5 <converse_ivan@hotmail.com>
2019-06-04 17:16:20 -0500

    Add Camera component and make some changes

---
+++ src/components/Camera.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="camera">
+    <video autoplay></video>
+    <button>SANP</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Camera"
+};
+</script>
+
+<style>
+</style>
